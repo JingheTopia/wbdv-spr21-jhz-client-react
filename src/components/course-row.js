a0c69d09d1c24059b2e1860f2c5ec2f584505a21
Jinghe Zhang <jinghezhang@Jinghes-MacBook-Pro.local>
2021-02-22 23:27:14 -0600

    add course-row and course-table

---
+++ src/components/course-row.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react'
+import {Link} from "react-router-dom";
+
+const CourseRow = (
+    {   deleteCourse,
+        updateCourse,
+        course,
+    }) => {
+    const [editing, setEditing] = useState(false)
+    const [title, setTitle] = useState(course.title)
+
+    const saveTitle = () => {
+        setEditing(false)
+        const newCourse = {
+            ...course,
+            title: title
+        }
+        updateCourse(newCourse)
+    }
+
+  return (
+      <div className="container-fluid">
+      <div className="col-12 mx-auto table-container">
+          <tr>
+            <td>
+                <i className="far fa-file-alt col-md-auto text-primary"/>
+                {
+                    !editing &&
+                    <Link to="/editor">
+                        {course.title}
+                    </Link>
+                }
+                {
+                    editing &&
+                    <input
+                        onChange={(event) => setTitle(event.target.value)}
+                        value={title}
+                        className="form-control"/>
+                }
+            </td>
+            <td class="list-title-col-content-1 d-none d-md-table-cell" >{course.owner}</td>
+            <td class="list-title-col-content-1 d-none d-lg-table-cell" >{course.lastModified}</td>
+
+              <td className="icon-col">
+                  {!editing && <i onClick={() => setEditing(true)} className="fas fa-edit"/>}
+                  {editing && <i onClick={() => saveTitle()} className="fas fa-check"/>}
+              </td>
+              <td className="icon-col">
+                  {editing && <i onClick={() => {
+                      deleteCourse(course)
+                      setEditing(false)
+                  }} className="fas fa-trash"/>
+                  }
+              </td>
+              <td className="icon-col">
+              </td>
+          </tr>
+      </div>
+      </div>
+  )
+}
+export default CourseRow
---
+++ src/components/course-table.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import CourseRow from "./course-row";
+import {Link} from "react-router-dom";
+
+export default class CourseTable
+  extends React.Component {
+
+  constructor(props) {
+    super(props)
+    console.log(props)
+  }
+
+  render() {
+    return(
+      <div>
+          <nav className="navbar navbar-expand-lg bg-light sticky-top">
+              <div className="container-fluid">
+                  <div className="col-12 mx-auto table-container">
+                      <table className="table">
+                          <tr>
+                              <th scope="col"
+                                  className="list-title-col d-xs-table-cell">Title
+                              </th>
+                              <th scope="col"
+                                  className="list-title-col-1 d-none d-md-table-cell">Owned By
+                              </th>
+                              <th scope="col"
+                                  className=" list-title-col-1 d-none d-lg-table-cell">Last modified by me
+                              </th>
+                              <th scope="col"
+                                  className="icon-col">
+                                  <a href="#">
+                                      <i className="fas fa-folder"/>
+                                  </a>
+                              </th>
+                              <th scope="col"
+                                  className="icon-col">
+                                  <a href="#">
+                                      <i className="fas fa-sort-alpha-down"/>
+                                  </a>
+                              </th>
+                              <th scope="col"
+                                  className="icon-col">
+                                  <Link to="/courses/grid">
+                                      <i className="fas fa-th"/>
+                                  </Link>
+                              </th>
+
+                          </tr>
+                      </table>
+                  </div>
+              </div>
+          </nav>
+
+        <table className="table">
+          <tbody>{
+            this.props.courses.map((course) =>
+              <CourseRow
+                updateCourse={this.props.updateCourse}
+                deleteCourse={this.props.deleteCourse}
+                key={course._id}
+                course={course}
+                title={course.title}
+                owner={course.owner}
+                lastModified={course.lastModified}
+              />)
+          }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
